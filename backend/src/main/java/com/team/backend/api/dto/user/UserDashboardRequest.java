d2223f82788ca129cd254b2a4ec07465524cea09
heath <a01080809887@gmail.com>
2026-01-13 00:27:09 +0900

    feature : userDashboard 필드값 삭제

--- backend/src/main/java/com/team/backend/api/dto/user/UserDashboardRequest.java
+++ backend/src/main/java/com/team/backend/api/dto/user/UserDashboardRequest.java
@@ -1,20 +1,15 @@
+// src/main/java/com/team/backend/api/dto/user/UserDashboardRequest.java
 package com.team.backend.api.dto.user;
-
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @NoArgsConstructor
 public class UserDashboardRequest {
-    /**
-     * CLICK_ITEMS | FAVORITED_CLICK_ITEMS | CATEGORY_DONUT | OUTFITS_DAILY
-     */
-    private String section;
 
-    /**
-     * optional. 미지정 시 KST 기준 현재 년/월
-     */
     private Integer year;
     private Integer month;
 }
